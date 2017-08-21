b982e9f5e8f6aaae8790797ec01053a692ab8718
klopez91 <31086446+klopez91@users.noreply.github.com>
2017-08-21 11:45:08 -0400

    updated changes

--- final deliverable/Program.cs
+++ final deliverable/Program.cs
@@ -10,16 +10,16 @@ namespace final_deliverable
 	{
 		static void Main(string[] args)
 		{
-			string i, j;
+			string a, b;
 			Console.WriteLine("Please enter a positive integer");
-			i = Console.ReadLine();
+			a = Console.ReadLine();
 			bool repeat = true; bool repeat2 = true;//to repeat while loop to catch for errors without throwing exception
 			while (repeat)
 			{
 				repeat = false;
 				try
 				{
-					int m = Int32.Parse(i);//have to hit enter twice if enter valid entry first time
+					int m = Int32.Parse(a);//have to hit enter twice if enter valid entry first time
 					uint p = (uint)(m);//to validate input is positive
 				}
 				catch (FormatException)
@@ -44,13 +44,13 @@ namespace final_deliverable
 				}
 			}
 			Console.WriteLine("Please enter a 2nd positive integer with the same number of digits as the first");
-			j = Console.ReadLine();
+			b = Console.ReadLine();
 			while (repeat2)
 			{
 				repeat2 = false;
 				try
 				{
-					int o = Int32.Parse(j);
+					int o = Int32.Parse(b);
 					uint q = (uint)(o);
 				}
 				catch (FormatException)
@@ -74,19 +74,51 @@ namespace final_deliverable
 					repeat2 = false;
 				}
 			}
-			if (i.Length == j.Length)//to validate that input 2 has same digits as input 1
+			if (a.Length == b.Length)//to validate that input 2 has same digits as input 1
 				repeat2 = false;
 			else
 				Console.WriteLine("Value does not match the number of digits from first, Please try again");
 			repeat2 = true;
+
 			Console.ReadLine();
-			int l, k;
-			l = Int32.Parse(i);//assinged new values to converted string 
-			k = Int32.Parse(j);
-			if (((k % 10) + (l % 10)) == ((k /= 10) + (l /= 10)))
-				Console.WriteLine("True, values are equal");
-			else
-				Console.WriteLine("False, values are not equal");
+			int newL, newK;
+			newL = Int32.Parse(a);//assinged new values to converted string 
+			newK = Int32.Parse(b);
+			if (//unable to return values of transformation and transformation again
+
+				Program.transformation(int)+Program.transformationAgain(int)== (newL % 10) + (newK % 10))
+			{
+				Console.WriteLine("True, Sum of digitis in same place add to the same sum");
+			}
+			else Console.WriteLine("False, sume of digits in same place do not add to the same sum");
+		}
+		public static int[] transformation(int x)
+		{
+			string a, numbers = x.ToString();
+			int[] num = numbers.Split(';').Select(int.Parse).ToArray();
+			int L = num.Length;
+			int[] x1 = new int[L];
+			for (int i = 0; i < L; i++)
+			{
+				a = num[i].ToString();
+				x1[i] = Convert.ToInt32(a);
+			}
+
+			return x1;
+		}
+		public static int[] transformationAgain(int y)
+		{
+			string b, numbers = y.ToString();
+			int[] num = numbers.Split(';').Select(int.Parse).ToArray();
+			int K = num.Length;
+			int[] y1 = new int[K];
+			for (int i = 0; i < K; i++)
+			{
+				b = num[i].ToString();
+				y1[i] = Convert.ToInt32(b);
+			}
+
+			return y1;
 		}
 	}
 }
